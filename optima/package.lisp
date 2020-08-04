005c73a05827f0168954078a28c133870f8e89a9
Masataro Asai <guicho2.71828@gmail.com>
2020-08-04 10:11:49 -0400

    erlang-term-optima aliases to erlang-term-trivia

--- optima/package.lisp
+++ optima/package.lisp
@@ -1,9 +1,15 @@
 (defpackage #:erlang-term-optima
-  (:documentation "Optima extension for matching Erlang terms.")
+  (:documentation "(Legacy) Transitional system for optima extensions for matching Erlang terms")
   (:nicknames #:etf-optima)
-  (:use #:cl #:erlang-term #:etf-bops #:optima #:optima.core)
+  (:use #:erlang-term-trivia)
   (:export
 
    #:erlang-string
 
    ))
+
+(alexandria:simple-style-warning
+ "Package ERLANG-TERM-OPTIMA is deprecated, due to the retirement of optima (removed from quicklisp).
+Consider switching to ERLANG-TERM-TRIVIA")
+
+;; 
